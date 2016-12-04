6d335868761ae5df9443a2ab1ec608666caa68c1
503945930 <503945930@qq.com>
2016-12-04 22:20:39 +0800

    learn vue

---
+++ src/components/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <h1>{{about}}</h1>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'about',
+    data () {
+      return {
+        about: 'hello'
+      }
+    }
+  }
+</script>
